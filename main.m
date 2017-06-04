7a26b48cce1514a5d8d0da62c92819fd086bb351
Amin Zabardast <AminZabardast@gmail.com>
2017-06-05 02:06:19 +0300

    "Adding comments for explanations"

--- main.m
+++ main.m
@@ -3,8 +3,10 @@ clc;
 original = imread('images/audreyhepburn.png');
 image = rgb2gray(original);
 image = double(image);
+% Key point coordinated, orientation and magnitute
 result = SIFT(image,3,5,1.6);
 for i=1:4:length(result)
+    % adding circle and lines to key point locations
     original = insertShape(original,'circle',[result(i+1),result(i),result(i+3)],'LineWidth',1,'color',[255,0,0],'SmoothEdges',false);
     original = insertShape(original,'line'...
         ,[result(i+1),result(i),result(i+1)+result(i+3)*sin(result(i+2)),result(i)+result(i+3)*cos(result(i+2))],...
