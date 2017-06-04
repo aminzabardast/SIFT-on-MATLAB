0d1b5a6c816391d98a45738d6c188778f9561823
Amin Zabardast <AminZabardast@gmail.com>
2017-06-05 02:03:05 +0300

    "Modifiying main code to fit new SIFT function. Drawing Circles and Lines for showing key points."

--- main.m
+++ main.m
@@ -4,8 +4,12 @@ original = imread('images/audreyhepburn.png');
 image = rgb2gray(original);
 image = double(image);
 result = SIFT(image,3,5,1.6);
-for i=1:2:length(result)
-    original(result(i),result(i+1),:) = [255,0,0];
+for i=1:4:length(result)
+    original = insertShape(original,'circle',[result(i+1),result(i),result(i+3)],'LineWidth',1,'color',[255,0,0],'SmoothEdges',false);
+    original = insertShape(original,'line'...
+        ,[result(i+1),result(i),result(i+1)+result(i+3)*sin(result(i+2)),result(i)+result(i+3)*cos(result(i+2))],...
+        'LineWidth',1,'color',[255,0,0]);
+    original(result(i),result(i+1),:) = [0,255,0];
 end
 figure
 imshow(uint8(original))
