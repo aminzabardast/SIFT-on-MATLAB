7d6a876f139f6ddde1ec128f046707bc212b3c3f
Amin Zabardast <AminZabardast@gmail.com>
2017-06-06 00:56:10 +0300

    "Main file changed to fit new SIFT function."

--- main.m
+++ main.m
@@ -1,22 +1,9 @@
 clear;
 clc;
-original = imread('images/sky.jpg');
+original = imread('images/audreyhepburn.png');
 image = rgb2gray(original);
 output = cat(3, image, image, image);
 image = double(image);
-% Key point coordinated, orientation and magnitute
-result = SIFT(image,3,5,1.6);
-for i=1:4:length(result)
-    % adding circles to key point locations
-    output = insertShape(output,'circle',[result(i+1),result(i),5],'LineWidth',1,'color',[255,0,0],'SmoothEdges',false);
-end
-for i=1:4:length(result)
-    % adding lines to key point locations
-    output = insertShape(output,'line',[result(i+1),result(i),result(i+1)+10*sind(result(i+2)),result(i)+10*cosd(result(i+2))],'LineWidth',1,'color',[0,0,255]);
-end
-for i=1:4:length(result)
-    % Distinguishing key point location with green dots
-    output(result(i),result(i+1),:) = [0,255,0];
-end
+[keyPoints, image] = SIFT(image,3,5,1.6);
 figure
-imshow(uint8(output))
+imshow(image)
