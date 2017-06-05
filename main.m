d472ca75ede1cea92b1ebf31ee96a074ba5aa9d3
Amin Zabardast <AminZabardast@gmail.com>
2017-06-06 01:02:30 +0300

    "Minor Fix."

--- main.m
+++ main.m
@@ -2,7 +2,6 @@ clear;
 clc;
 original = imread('images/audreyhepburn.png');
 image = rgb2gray(original);
-output = cat(3, image, image, image);
 image = double(image);
 [keyPoints, image] = SIFT(image,3,5,1.6);
 figure
