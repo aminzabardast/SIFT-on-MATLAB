f3e3b7f76b477ef90fec27ff261bbd895e2b4597
Amin Zabardast <AminZabardast@gmail.com>
2017-06-05 16:41:37 +0300

    "Added function to calculate Gaussian Kernel using radius and Standard Deviation."

--- SIFT.m
+++ SIFT.m
@@ -135,3 +135,23 @@ function v = atan3d(y,x)
         v = 360 + v;
     end
 end
+
+%% Calculating Gaussian value given SD
+function result = gaussianKernel(SD, Radius)
+% Returns a gaussian kernet
+% By default a radius will be chosen to so kernel covers 99.7 % of data.
+    if nargin < 2
+        Radius = ceil(3*SD);
+    end
+    side = 2*Radius+1;
+    result = zeros(side);
+    for i = 1:side
+        for j = 1:side
+            x = i-(Radius+1);
+            y = j-(Radius+1);
+            result(i,j)=(x^2+y^2)^0.5;
+        end
+    end
+    result = exp(-(result .^ 2) / (2 * SD * SD));
+    result = result / sum(result(:));
+end
